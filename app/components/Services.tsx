4023ca32ff968ccbcf55a954ff0986614f3b3b4a
Owlbeard <omeraltun96@gmail.com>
2023-06-19 17:44:53 +0300

    Change font size of Services header

--- app/components/Services.tsx
+++ app/components/Services.tsx
@@ -15,7 +15,7 @@ export default function Hero() {
         <m.h1
           animate={{ x: 0 }}
           initial={{ x: '-120%' }}
-          className="shade text-4xl md:text-6xl font-extrabold drop-shadow-lg"
+          className="shade text-3xl md:text-6xl font-extrabold drop-shadow-lg"
         >
           İşte Sürdürülebilir Hizmetlerimizden Sadece Birkaçı!
         </m.h1>
